968234596a31b575c31967ff01257624f647295a
Rkhajuriwala <rishi.khajuriwala@gmail.com>
2018-04-21 17:38:27 -0400

    changes the constraints

--- plotSimulation.m
+++ plotSimulation.m
@@ -6,29 +6,31 @@ outputs = logsout.getElement('outputs');
 Y = outputs.Values.Data(:,1); % Y position
 Z = outputs.Values.Data(:,2); % yaw angle
 
-figure('position',[100 100 800 720])
-
-subplot(3,1,1)
+% f= figure('position',[100 100 800 720]);
+hold on
+% subplot(3,1,1)
 [~,Zref] = plotReference(Vx,time);
-hold on
-plot(outputs.Values.time*Vx,Y,'g','LineWidth',2)
-legend('reference','obstacles','mpc')
-grid on
+% hold on
+% plot(outputs.Values.time*Vx,Y,'LineWidth',2)
+% legend('reference','obstacles','mpc')
+% grid on
 
-subplot(3,1,2)
-plot(time,Zref,'b','LineWidth',2)
-hold on
-plot(outputs.Values.time,Z,'g','LineWidth',2)
-xlabel('time (s)')
-ylabel('yaw angle (rad)')
-legend('reference','mpc')
-grid on
-
-subplot(3,1,3)
-plot(time,steering.Values.Data,'g','LineWidth',2)
+% subplot(3,1,2)
+% hold on
+% plot(time,Zref,'b','LineWidth',2)
+% hold on
+% plot(outputs.Values.time,Z,'LineWidth',2)
+% xlabel('time (s)')
+% ylabel('yaw angle (rad)')
+% legend('reference','mpc')
+% grid on
+% 
+% subplot(3,1,3)
+% hold on
+plot(time,steering.Values.Data,'LineWidth',2)
 xlabel('time (s)')
 ylabel('steering angle (rad)')
-legend('mpc')
+% legend('mpc')
 grid on
 
 
